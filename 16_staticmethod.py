f3f49336ccd815d4eeab8a456aada9b061013fb4
Pavel Vavilin <shtartora@gmail.com>
2022-02-05 09:07:38 +0500

    Webinar 1.2

--- 16_staticmethod.py
+++ 16_staticmethod.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
+# staticmethod
+
 import os
+
 class Executor:
     def __init__(self, command):
         self.command = command
     @staticmethod
     def chdir(path):
         os.chdir(path)
-
+    def __call__(self):
+        os.system(self.command)
 orig_path = os.getcwd()
 print(orig_path)
+executor = Executor("ls|wc -l")
 Executor.chdir("/tmp/")
+print(executor())
 print(os.getcwd())
-Executor("test").chdir(orig_path)
+executor.chdir(orig_path)
 print(os.getcwd())
+print(executor())
